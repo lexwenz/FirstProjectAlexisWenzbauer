3ba0d934c81613fc51440108bec22113c1bae84b
lexiw <lexiw@DESKTOP-DQ0EHIV>
2018-08-29 23:06:11 -0600

    Got my program working!

---
+++ src/firstprojectalexiswenzbauer/FirstProjectAlexisWenzbauer.java
@@ -0,0 +1,14 @@
+//Alexis Wenzbauer
+package firstprojectalexiswenzbauer;
+
+public class FirstProjectAlexisWenzbauer
+{
+    public static void main(String[] args)
+    {
+        String myMessage = "    J      A      V     V     A\n"
+                         + "    J     A A      V   V     A A\n"
+                         + "J   J    AAAAA      V V     AAAAA\n"
+                         + " J J    A     A      V     A     A";
+        System.out.println(myMessage);
+    }
+}
